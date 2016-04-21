8d2600b23655a66a808fa2933879e880e614233a
Brian Smith <432a234@gmail.com>
2016-04-20 20:39:21 -0700

    Add todo model and POST /todos

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -3,6 +3,7 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
     'dialect': 'sqlite',
     'storage': __dirname + '/basic-sqlite-database.sqlite'
 });
+
 var Todo = sequelize.define('todo', {
     description: {
         type: Sequelize.STRING,
@@ -17,43 +18,48 @@ var Todo = sequelize.define('todo', {
         defaultValue: false
     }
 });
+
 sequelize.sync({
-    force: true
+    // force: true
 }).then(function() {
     console.log('Everything is synced');
 
-  // Todo.findById(3).then(function (todo) {
-  // 	if (todo) {
-  // 		console.log(todo.toJSON());
-  // 	} else {
-  // 		console.log('Todo not found');
-  // 	}
-  // });
 
-    Todo.create({
-        description: 'Take out trash'
-    }).then(function(todo) {
-        return Todo.create({
-            description: 'Clean office'
-        });
-    }).then(function() {
-        // return Todo.findById(1)
-        return Todo.findAll({
-            where: {
-                description: {
-                	$like: '%Office%'	
-                }
-            }
-        });
-    }).then(function(todos) {
-        if (todos) {
-            todos.forEach(function(todo) {
-                console.log(todo.toJSON());
-            });
-        } else {
-            console.log('No todo found!');
-        }
-    }).catch(function(e) {
-        console.log(e);
-    })
+  Todo.findById(3).then(function (todo) {
+  	if (todo) {
+  		console.log(todo.toJSON());
+  	} else {
+  		console.log('Todo not found');
+  	}
+  });
+
+
+    // Todo.create({
+    //     description: 'Take out trash'
+    // }).then(function(todo) {
+    //     return Todo.create({
+    //         description: 'Clean office'
+    //     });
+    // }).then(function() {
+    //     // return Todo.findById(1)
+    //     return Todo.findAll({
+    //         where: {
+    //             description: {
+    //             	$like: '%Office%'	
+    //             }
+    //         }
+    //     });
+    // }).then(function(todos) {
+    //     if (todos) {
+    //         todos.forEach(function(todo) {
+    //             console.log(todo.toJSON());
+    //         });
+    //     } else {
+    //         console.log('No todo found!');
+    //     }
+    // }).catch(function(e) {
+    //     console.log(e);
+    // })
+    
+
 });
