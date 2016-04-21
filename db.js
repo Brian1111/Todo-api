db1563d5e95dc4eaef964445f74a9168b5d3db16
Brian Smith <432a234@gmail.com>
2016-04-21 13:01:47 -0700

    Fixed bug

--- db.js
+++ db.js
@@ -3,18 +3,14 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
 if (env === 'production') {
-<<<<<<< d6560df92f7d8bcd28d8073101382c1d381ddbbe
-    sequelize = new Sequelize(process.env.DATABASE_URL {
-=======
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
->>>>>>> Convert GET /todos to sequelize
-        dialect: 'postgres'
-    });
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
 } else {
-    sequelize = new Sequelize(undefined, undefined, undefined, {
-        'dialect': 'sqlite',
-        'storage': __dirname + '/data/dev-todo-api.sqlite'
-    });
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
 }
 
 var db = {};
